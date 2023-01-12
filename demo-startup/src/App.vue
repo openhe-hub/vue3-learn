e0906f0f4b653c1a3f9c9427dc2380069bc67033
openhe <625529334@qq.com>
2023-01-13 00:42:43 +0800

    1-12-study: template grammar & basic component

--- demo-startup/src/App.vue
+++ demo-startup/src/App.vue
@@ -1,29 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  // properties
+  data() {
+    return {
+      num: 0,
+      username: "openhe",
+      msg:"<h2>raw html</h2>",
+      id:"box",
+      attr:"id"
+    }
+  },
+  // methods
+  methods:{
+    addNum(){
+      this.num++;
+    }
+  },
+  // computed properties
+  computed:{
+    reverseMsg(){
+      return this.msg.split('').reverse().join('');
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p>{{ num }}</p>
+    <p v-once>{{ num }}</p>
+    <p>{{ username }}</p>
+    <button @click="addNum">num+1</button>
+    <div v-html="msg"></div>
+    <div :[attr]="id">v-model</div>
+    <div>reversed msg(use computed property) = {{reverseMsg}} </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- docs/note1.md
+++ docs/note1.md
@@ -15,4 +15,49 @@
    npm install
    npm run dev
    ```
-4. 
+4. 一个简单的组件demo
+   ```html
+   <script>
+   export default{
+      data(){
+         return{
+            num:0,
+            username:"openhe"
+         }
+      }
+   }
+   </script>
+
+   <template>
+   <div>
+   <p>{{num}}</p>
+   <p>{{username}}</p>
+   </div>
+   </template>
+
+   <style scoped>
+
+   </style>
+
+   ```
+5. 基础模板语法&指令
+   1. mustache插值语法：`<p>{{ num }}</p>`
+   2. v-once, 不更新插值数据：`<p v-once>{{ num }}</p>`
+   3. v-html, 原始html：`<div v-html="msg"></div>`
+   4. v-bind, 属性绑定：`<div v-bind:id="id">v-model</div>`, 语法糖: `:id`
+   5. v-on, 事件监听：`<button @click="addNum">num+1</button>`, 语法糖: `@click`
+   6. 动态属性: `<div :[attr]="id">v-model</div>`
+   7. 动态事件，参考动态属性，略
+6. data：组件属性
+7. methods：组件方法
+8. computed：计算属性
+   ```javascript
+   computed:{
+    reverseMsg(){
+      return this.msg.split('').reverse().join('');
+    }
+   ```
+   使用：同mustache插值语法
+   * 性能提升，只要计算属性的依赖值不变，不会重复计算
+   * 计算属性默认只有getter,但是也可以手动添加setter(一般只用于只读属性)
+9.  
